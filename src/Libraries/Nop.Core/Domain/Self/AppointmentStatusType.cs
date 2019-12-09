042e0fb7f438297e6ecf9c694af96b9833c8e8be
david <david@buywell.com>
2019-12-09 18:46:12 -0500

    create appointment class

---
+++ src/Libraries/Nop.Core/Domain/Self/AppointmentStatusType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Core.Domain.Self
+{
+    enum AppointmentStatusType
+    {
+        free,
+        waiting,
+        confirmed
+    }
+}
---
+++ src/Libraries/Nop.Data/Mapping/Self/ProductAppointmentMap.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nop.Core.Domain.Self;
+
+namespace Nop.Data.Mapping.Self
+{
+    public partial class ProductAppointmentMap : NopEntityTypeConfiguration<ProductAppointment>
+    {
+        #region Methods
+
+        /// <summary>
+        /// Configures the entity
+        /// </summary>
+        /// <param name="builder">The builder to be used to configure the entity</param>
+        public override void Configure(EntityTypeBuilder<ProductAppointment> builder)
+        {
+            builder.ToTable("tblProductAppointment");
+            builder.HasKey(productAppointment => productAppointment.Id);
+
+            builder.HasOne(productAppointment => productAppointment.Product)
+                .WithMany(product => product.ProductAppointments)
+                .HasForeignKey(productAppointment => productAppointment.ProductId)
+                .IsRequired();
+
+            builder.HasOne(productAppointment => productAppointment.Customer)
+                .WithMany()
+                .HasForeignKey(productAppointment => productAppointment.CustomerId)
+                .IsRequired();
+
+            base.Configure(builder);
+        }
+
+        #endregion
+    }
+}
