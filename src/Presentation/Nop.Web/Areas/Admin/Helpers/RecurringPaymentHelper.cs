53897bb8e98fb50593ee1b540f6ac92ccb6072b6
david <david@buywell.com>
2019-12-17 16:48:53 -0500

    Get next payment date helper

---
+++ src/Presentation/Nop.Web/Areas/Admin/Helpers/RecurringPaymentHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Nop.Core.Domain.Catalog;
+
+namespace Nop.Web.Areas.Admin.Helpers
+{
+    public static class RecurringPaymentHelper
+    {
+        public static DateTime GetNexDate(DateTime startDate, RecurringProductCyclePeriod cyclePeriod)
+        {
+            DateTime today = DateTime.Today;
+            DateTime nextDate = today.AddDays(1);
+            switch (cyclePeriod)
+            {
+                case RecurringProductCyclePeriod.Months:
+                    var months = 12 * (today.Year - startDate.Year) + today.Month - startDate.Month;
+                    if (today.Date >= startDate.Date)
+                    {
+                        months++;
+                    }
+                    nextDate = startDate.AddMonths(months);
+                    break;
+                case RecurringProductCyclePeriod.Years:
+                    var years = (today - startDate).TotalDays / 365.25;
+                    nextDate = startDate.AddYears((int)Math.Ceiling(years));
+                    break;
+                case RecurringProductCyclePeriod.Weeks:
+                    var days = (today - startDate).TotalDays;
+                    nextDate = startDate.AddDays(days);
+                    break;
+            }
+
+            return nextDate;
+        }
+    }
+}
